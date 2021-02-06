3e2c0ded0b493a8c1b0eeecd2df0db5b34c23530
George Henne <george.henne@gmail.com>
2021-02-06 03:44:47 -0500

    Change recipes that used a creosote bucket to use the fluid tag for creosote and immersiveengineering:shaped_fluid instead.

--- src/main/java/zed/d0c/floormats/plugin/JEIPlugin.java
+++ src/main/java/zed/d0c/floormats/plugin/JEIPlugin.java
@@ -1,24 +1,18 @@
 package zed.d0c.floormats.plugin;
 
-import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.ingredients.IIngredientHelper;
-import mezz.jei.api.ingredients.subtypes.UidContext;
-import mezz.jei.api.registration.IModIngredientRegistration;
 import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.registries.ForgeRegistries;
 import zed.d0c.floormats.FloorMats;
 
 import javax.annotation.Nonnull;
-
 import java.util.Collections;
 
-import static zed.d0c.floormats.setup.Registration.*;
+import static zed.d0c.floormats.setup.Registration.GILDED_TREATED_WOOD_FLOORMAT_ITEM;
+import static zed.d0c.floormats.setup.Registration.TREATED_WOOD_FLOORMAT_ITEM;
 
 @mezz.jei.api.JeiPlugin
 public class JEIPlugin implements IModPlugin {
@@ -31,18 +25,15 @@ public class JEIPlugin implements IModPlugin {
         return UID;
     }
 
-    //    static IModIngredientRegistration iModIngredientRegistration;
-    /*
     @Override
-    public void registerIngredients(@Nonnull IModIngredientRegistration registration) {
-        iModIngredientRegistration = registration;
-    }
-    */
-
-    @Override
-    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
-        IIngredientManager manager = jeiRuntime.getIngredientManager();
+    public void onRuntimeAvailable(@Nonnull IJeiRuntime jeiRuntime) {
+//      // Was going to base treated wood off the existence of an entry in this fluid tag, but instead went with IE
+//      // needs to exist.
+//      ITag<Fluid> creosoteTag = FluidTags.getCollection().get(new ResourceLocation("forge", "creosote"));
+//      if ((creosoteTag == null) || creosoteTag.getAllElements().isEmpty()) {
+        //noinspection SpellCheckingInspection
         if (!ModList.get().isLoaded("immersiveengineering")) {
+            IIngredientManager manager = jeiRuntime.getIngredientManager();
             manager.removeIngredientsAtRuntime(VanillaTypes.ITEM, Collections.singleton(TREATED_WOOD_FLOORMAT_ITEM.get().getDefaultInstance()));
             manager.removeIngredientsAtRuntime(VanillaTypes.ITEM, Collections.singleton(GILDED_TREATED_WOOD_FLOORMAT_ITEM.get().getDefaultInstance()));
         }
